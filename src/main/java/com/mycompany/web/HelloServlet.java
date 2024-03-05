bb78809c94ca5a5ef7ffcc07b5ef27752cb24f3e
Ubuntu <ubuntu@ip-172-31-88-104.ec2.internal>
2024-03-05 17:50:50 +0000

    first commit

---
+++ src/main/java/com/mycompany/web/HelloServlet.java
@@ -0,0 +1,18 @@
+package com.mycompany.web;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class HelloServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        out.println("<h2>Hello, Maven and Jenkins Deployment!</h2>");
+        out.println("</body></html>");
+    }
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,9 @@
+<html>
+<head>
+    <title>Maven Jenkins Deployment Example</title>
+</head>
+<body>
+    <h2>Welcome to our Maven Jenkins Deployment Example</h2>
+    <p>${message}</p>
+</body>
+</html>
